509c712aeaa20a041be52eb9afa62631ee9e6b9f
mehni <lozomehni28@gmail.com>
2022-06-13 10:00:25 +0100

    swagger live documentation synchronization activated

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+
+  const options = new DocumentBuilder()
+      .setTitle('Jira API')
+      .setDescription('Backend API')
+      .setVersion('1.0.0')
+      .addBearerAuth(
+        { type: 'http', scheme: 'bearer', bearerFormat: 'Token' },
+        'access-token',
+      )
+      .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api', app, document);
+
   console.log(process.env.DB_HOST);
   await app.listen(3000);
 }
