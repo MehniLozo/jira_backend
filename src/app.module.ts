28616ba1439fd296fc916228eac11bd0bd926bfb
mehni <lozomehni28@gmail.com>
2022-06-13 08:54:39 +0100

    some port stuff

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import  {typeOrmAsyncConfig} from './config/typeorm.config';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import {UserModule} from './modules/user/user.module';
+import {ProjectModule} from './modules/project/project.module';
+import {IssueModule} from './modules/issue/issue.module';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
+    EventEmitterModule.forRoot(),
+    UserModule,
+    ProjectModule,
+    IssueModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
