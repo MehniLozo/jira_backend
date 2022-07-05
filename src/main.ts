bc22981fc8942bd73546b1a5505cb8779a3b3c14
mehni <lozomehni28@gmail.com>
2022-07-05 09:44:58 +0100

    cors stuff for the frontend

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ import appDataSource from './config/app-data-source';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.enableCors();
 
   const options = new DocumentBuilder()
       .setTitle('Jira API')
