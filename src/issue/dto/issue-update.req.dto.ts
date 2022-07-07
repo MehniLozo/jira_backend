e2bc2d1a1ced53ecc5a8e28c82830ed1bc952fb1
mehni <lozomehni28@gmail.com>
2022-07-07 12:23:25 +0100

    Issue update dto validator

--- src/issue/dto/issue-update.req.dto.ts
+++ src/issue/dto/issue-update.req.dto.ts
@@ -1,41 +1,54 @@
-import { IsOptional } from 'class-validator';
+import { IsEnum, IsInt, IsOptional, IsString, Length } from 'class-validator';
 import { IssueType, IssueStatus, IssuePriority } from '../issues.constants';
 import { User } from '../../user/user.entity';
 
 export class IssueUpdateRequestDto {
   @IsOptional()
+  @IsString()
+  @Length(10, 20)
   description?: string;
 
   @IsOptional()
+  @IsEnum(IssuePriority)
   priority?: IssuePriority;
 
   @IsOptional()
+  @IsInt()
   projectId?: number;
 
   @IsOptional()
+  @IsInt()
   reporterId?: number;
 
   @IsOptional()
+  @IsEnum(IssueStatus)
   status?: IssueStatus;
 
   @IsOptional()
+  @IsString()
+  @Length(10, 20)
   title?: string;
 
   @IsOptional()
+  @IsEnum(IssueType)
   type?: IssueType;
 
   @IsOptional()
   userIds?: number[];
 
   @IsOptional()
+  @IsInt()
   users?: User[];
 
   @IsOptional()
+  @IsInt()
   listPosition?: number;
 
   @IsOptional()
-  estimate?: number | null;
+  @IsInt()
+  estimate?: number;
 
   @IsOptional()
-  timeRemaining?: number | null;
+  @IsInt()
+  timeRemaining?: number;
 }
