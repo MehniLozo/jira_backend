b7835a00a1fdeb4165350658827c3a37d67a774b
mehni <lozomehni28@gmail.com>
2023-05-07 20:25:13 +0200

    more docs

--- src/project/dto/project-register.req.dto.ts
+++ src/project/dto/project-register.req.dto.ts
@@ -1,22 +1,27 @@
 import { ProjectCategory } from '../project.constants';
 import { IsNotEmpty, Length, IsUrl, IsEnum, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class ProjectRegisterRequestDto {
   @IsNotEmpty()
   @Length(5, 20)
+  @ApiProperty({ type: String })
   name: string;
 
   @IsNotEmpty()
   @IsUrl()
   @IsString()
+  @ApiProperty({ type: String })
   url: string;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({ type: String })
   description: string;
 
   @IsNotEmpty()
   @IsEnum(ProjectCategory)
+  @ApiProperty({ type: String })
   category: ProjectCategory;
 
   userIds: number[];
--- src/user/dto/user-login.req.dto.ts
+++ src/user/dto/user-login.req.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class UserloginRequestDto {
   @IsNotEmpty()
+  @ApiProperty({ type: String })
   @IsString()
   username: string;
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({ type: String })
   password: string;
 }
